f6588aece2801ff574a13570bfc3dba8f2925c0b
Nicolas Helgeson <afkupuz72@yahoo.com>
2015-11-23 14:14:24 -0800

    extra credit additions
    enhanced functionality

--- tournament.sql
+++ tournament.sql
@@ -6,31 +6,39 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Drop database if it already exists so that we ensure everything is fresh and correct
+
 DROP DATABASE IF EXISTS tournament;
+
+-- Create database
+
 CREATE DATABASE tournament;
+
+-- connect to database
+
 \c tournament;
-CREATE TABLE IF NOT EXISTS players (
+
+-- create tables and define their units, and relationships
+-- players contain player names, their start date and if they are active
+
+CREATE TABLE players (
 	playerid	SERIAL,
-	name		varchar(40) NOT NULL,
+	name		text NOT NULL,
 	date_added	TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	active		integer,
-	win		integer,
-	total_matches	integer,
+	active		integer DEFAULT 1,
 	PRIMARY KEY (playerid)
 );
-CREATE TABLE IF NOT EXISTS matches (
-	matchid		SERIAL,
+
+-- matches are organized by matchid and players can join a matchid
+-- this allows multiple tournaments with different matchids
+
+CREATE TABLE matches (
+	matchid		integer DEFAULT 0,
 	playerid	integer REFERENCES players,
-	win		integer,
-	loss		integer,
-	place		integer,
-	champion	integer,
-	bye		integer,
+	win		integer DEFAULT 0,
+	total_matches	integer DEFAULT 0,
+	score		integer DEFAULT 0,
+	played		integer[],
+	bye		integer DEFAULT 0,
 	PRIMARY KEY (matchid, playerid)
-);
-CREATE TABLE IF NOT EXISTS stats (
-	playerid	integer NOT NULL REFERENCES players,
-	tournament_wins	integer,
-	game_wins	integer,
-	PRIMARY KEY (playerid)
 );
